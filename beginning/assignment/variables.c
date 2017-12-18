8e253e9336aeb7063542940c86d3ecacdcfe4dcd
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2017-12-18 18:36:43 +0530

    Create variables.c

---
+++ beginning/assignment/variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int a,d;
+float b;
+char c[]="Sachin Ramesh Tendulkar";
+char e[]="Little Master";
+a=18426;
+b=44.83;
+d=463;
+printf("%s\n%s\n%d\n%f\n%d",c,e,a,b,d);
+return 0;
+}
