51b8f4ab19d8fbb49c8509db85665cf19090fe0f
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2017-12-20 21:36:00 +0530

    Add files via upload

---
+++ practice/codekata/asignments/timedifference.c
@@ -0,0 +1,42 @@
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+int h1,m1,h2,m2,dif;
+
+scanf("%d %d",&h1,&m1);
+
+scanf("%d %d",&h2,&m2);
+
+h1=h1*60+m1;
+
+h2=h2*60+m2;
+
+dif=h2-h1;
+
+if(dif>60)
+
+{
+
+h1=dif/60;
+
+h2=dif%60;
+
+printf("time difference is %d hour %d minutes",h1,h2);
+
+}
+
+else
+
+{
+
+printf("time difference %d minutes",dif);
+
+}
+
+return 0;
+
+}
