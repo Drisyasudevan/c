a6b44ab8c6295970989f6622dd265a69d1313313
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2017-12-21 19:44:09 +0530

    Add files via upload

---
+++ practice/codekata/asignments/power_of2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+int a,i=1,j=1;
+
+scanf("%d",&a);
+
+if(a!=0)
+
+{
+
+while(j<=40)
+
+{
+
+i=i*2;
+
+if(a==i)
+
+{
+
+	printf("Yes");
+
+	break;
+
+}
+
+if(i>a)
+
+{
+	printf("No");
+
+	break;
+
+}
+
+}
+
+j++;
+
+}
+
+return 0;
+
+}
