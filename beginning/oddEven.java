87d3ad04b2d67833186a29818177a6bc9d466c00
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-02-21 15:52:39 +0530

    Add files via upload

---
+++ beginning/oddEven.java
@@ -0,0 +1,18 @@
+package gu;
+
+import java.util.*;
+public class OddEven {
+    public static void main(String args[]){
+    int n;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    if(n%2==0)
+    {
+        System.out.print("Even");
+    }
+    else
+    {
+      System.out.print("Odd");  
+    }
+}
+}
