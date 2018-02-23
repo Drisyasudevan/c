44030f79d669246ef7d3ac09751f50b54661166c
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-02-23 11:23:58 +0530

    Add files via upload

---
+++ beginning/hello.java
@@ -0,0 +1,15 @@
+package guv;
+
+import java.util.*;
+public class Hello {
+ public static void main(String a[])
+{
+int n,i;
+Scanner s=new Scanner(System.in);;
+n=s.nextInt();
+for(i=0;i<n;i++)
+{
+    System.out.print("Hello\n");
+}
+}   
+}
