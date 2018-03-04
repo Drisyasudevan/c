8a416038a163833065fb346cc522cead4af247d7
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-04 19:36:29 +0530

    Add files via upload

---
+++ beginning/append.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class Strings
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	String str;
+
+	Scanner s=new Scanner(System.in);
+
+	str=s.nextLine();
+
+	System.out.print(str+".");
+
+	}
+
+
+}
