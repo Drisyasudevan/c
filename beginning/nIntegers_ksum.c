286934d12f95c050b6b47dac3b72f37de9149667
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-04 12:48:25 +0530

    Add files via upload

---
+++ beginning/nIntegers_ksum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+int n,k,a[50],sum=0,i;
+
+scanf("%d %d",&n,&k);
+
+for(i=1;i<=n;i++)
+
+{
+
+	a[i]=i;
+
+	printf("%d ",a[i]);
+
+}
+
+for(i=1;i<=k;i++)
+
+{
+
+	sum=sum+i;
+
+}
+
+printf("\n%d",sum);
+
+	return 0;
+
+}
