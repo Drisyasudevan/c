82a28e7acf076ae91db35ea2e4f6f41611a39754
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-04 20:28:30 +0530

    Add files via upload

---
+++ beginning/even_2range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int n1,n2,i;
+  scanf("%d %d",&n1,&n2);
+  for(i=n1;i<=n2;i++)
+  {
+  	if(i%2==0)
+  	{
+  		printf("%d ",i);
+  	}
+  }
+
+    return 0;
+}
