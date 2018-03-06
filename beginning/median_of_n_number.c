672817d18bea26ab24688b9ae6e88160e3554345
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-06 21:50:18 +0530

    Add files via upload

---
+++ beginning/median_of_n_number.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+
+	int a[20],n,j,temp,i;
+
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++)
+
+	{
+
+	scanf("%d",&a[i]);
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		for(j=i+1;j<n;j++)
+
+	{
+
+	
+	if(a[i]>a[j])
+
+	{
+
+		temp=a[i];
+
+		a[i]=a[j];
+
+		a[j]=temp;
+
+	}
+
+	}
+
+	}
+
+	
+printf("%d ",a[n/2]);
+
+	
+return 0;
+
+}
