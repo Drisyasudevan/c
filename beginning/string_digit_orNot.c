585247ee018cab352b4a8c27e51364410a08cfee
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-06 22:00:30 +0530

    Add files via upload

---
+++ beginning/string_digit_orNot.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main(void)
+
+{
+
+	char str[50];
+
+    int i,len = 0,count = 0;
+
+    scanf("%s",str);
+
+    len = strlen(str);
+
+    for(i=0;i<len;i++)
+
+    {
+
+            if(str[i] >= 48 && str[i] <= 57)
+    
+            {
+
+                  count++;
+
+            }
+
+    }
+
+   if(count==len)
+
+   {
+
+   	printf("Yes");
+
+   }
+
+   else
+
+   {
+
+   	printf("No");
+
+   }
+
+	return 0;
+
+}
