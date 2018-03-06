303fef45e81012c08cfc10fa486c68714aaef21d
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-06 21:29:37 +0530

    Add files via upload

---
+++ beginning/sum_arithmeticProgression2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+
+	int i,n,a,d,sum=0,temp;
+
+	scanf("%d %d %d",&n,&a,&d);
+
+	temp=n;
+
+temp=a+(temp-1)*d;
+
+sum=(n*(a+temp))/2;
+
+	printf("%d",sum);
+
+	return 0;
+
+}
