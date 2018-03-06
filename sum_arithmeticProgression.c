202f5e67180a2f3e8a75c5989ca541677853d367
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-06 21:19:02 +0530

    Add files via upload

---
+++ sum_arithmeticProgression.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+	int i,n,a,d,sum=0;
+
+	scanf("%d %d %d",&n,&a,&d);
+
+	for(i=a;i<=n;i+d);
+
+	{
+
+		sum=sum+i;
+
+	}
+
+	printf("%d",sum);
+
+	return 0;
+
+}
