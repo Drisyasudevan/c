8efb76765069d7f606139a8d917b8e75973a9517
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-07 16:23:48 +0530

    Add files via upload

---
+++ beginning/min_to_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+int min,h,m,g,temp;
+
+scanf("%d",&min);
+
+temp=min;
+if(min<60)
+
+{
+
+	h=0;
+
+	m=min;
+
+}
+
+else
+
+{
+
+	h=min/60;
+
+	g=h*60;
+
+	m=temp-g;
+
+}
+
+printf("%d %d",h,m);
+
+return 0;
+
+}
