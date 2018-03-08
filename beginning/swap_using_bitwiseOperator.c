e012609f2454db03d6104a95dd3d40b93ba14d2a
Drisyasudevan <34640928+Drisyasudevan@users.noreply.github.com>
2018-03-08 10:05:29 +0530

    Add files via upload

---
+++ beginning/swap_using_bitwiseOperator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+ {
+  
+  int i;
+ 
+   int k;
+ 
+   scanf("%d %d",&i,&k);
+
+
+    i = i ^ k;
+ 
+   k = i ^ k;
+  
+  i = i ^ k;
+  
+  printf("%d %d",i,k);
+
+ 
+   return 0;
+
+}
+
